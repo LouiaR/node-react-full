ec23eea6885d860c8ff49e4e5ad7038f6cc1d025
louiaR <louitiani@gmail.com>
2018-06-26 02:23:09 +0100

    registration-user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,65 @@
-const express =require('express');
+const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// load user model
+const User = require('../../model/User');
 
 router.get('/test', (req, res) => res.json({
   greet: 'Bonjour users'
 }));
+
+// @route for sign up 
+//@access public
+
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({email: 'Email already exists.'});
+      } else {
+
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', //size
+          r: 'pg', //rating
+          d: 'mm' // Default
+        });
+
+        const newUser = new User ({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log('err'));
+          });
+        });
+      }
+    }); 
+});
+
+// router.post('./register', (req, res) => {
+
+//   const newUser = {
+//     name: req.body.name,
+//     email: req.body.email,
+//     password: req.body.password
+
+//   };
+//   res.json(newUser);
+    
+// });
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('combined'));
+
+// body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys').mongoURI;
@@ -18,7 +26,7 @@ mongoose
 
 app.get('/', (req, res) => {
   res.send('Hello Bonjour');
-})
+});
 
 // user Routes
 app.use('/api/users', users);
@@ -26,4 +34,4 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 const port = process.env.Port || 5000;
 
-app.listen(port, () => console.log(`Server running at ${port}`))
+app.listen(port, () => console.log(`Server running at ${port}`));
